81ea57d7cebc95881e5ac8cfe8e5682aa65fe26c
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-20 19:08:48 +0600

    Initial Commit

---
+++ TextViewWithHTMLContent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextViewWithHTMLContent
+//
+//  Created by ZEUS on 20/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
